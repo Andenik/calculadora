84811cae7378b7c8a2ce44364756a05560c2d368
Andenik <andenik@yahoo.com.br>
2024-07-19 23:34:43 -0300

    operacoes adicionadas com resultado

--- calcladora.py
+++ calcladora.py
@@ -15,10 +15,24 @@ while True:
     if numero_2.isdigit() == False:
         print("Voce nao digitou um numero tente novamente")
         continue
-
     operador = input("Digite um Operador +-/*: ")
+     #checa se o usuario digitou um operador correto
     if operador not in operadores:
         print('voce nao digitou um operador')
-    
+    #nao permite que o usuario digite mais de um operador
     if len(operador) > 1:
         print('Digite apenas um operador')
+
+    if operador == '+':
+        print(f'{float(numero_1) + float(numero_2)}')
+    elif operador == '-':
+        print(f'{float(numero_1) - float(numero_2)}')
+    elif operador == '/':
+        print(f'{float(numero_1) / float(numero_2)}')
+    elif operador == '*':
+        print(f'{float(numero_1) * float(numero_2)}')
+
+
+
+
+    
